961ef9bb6a9e0debf8a06bb0f27cd8cea34577e4
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-06-18 12:53:44 +0300

    Added populate-credhub job

---
+++ mysql/tasks/populate-credhub/task.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eu
+
+pushd bbl-state/${BBL_ENV}/state
+eval "$(bbl print-env)"
+popd
+CFPSW=$(credhub get -n /bosh-bbl-env-${BBL_ENV}/cf/cf_admin_password | grep ^value | awk '{print $2}')
+credhub set -n /bosh-bbl-env-${BBL_ENV}/prometheus/cf_admin_password -t value -v $CFPSW
