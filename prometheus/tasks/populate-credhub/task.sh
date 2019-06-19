5a9bb8edb3601ad415b6160b27cfeedae4e94e3c
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-06-19 20:22:24 +0300

    Added 2 more vars

--- prometheus/tasks/populate-credhub/task.sh
+++ prometheus/tasks/populate-credhub/task.sh
@@ -4,8 +4,15 @@ pushd bbl-state/${BBL_ENV}/state
 eval "$(bbl print-env)"
 popd
 set -x
-BBL_ENV_NAME="/bosh-bbl-env-${BBL_ENV}/prometheus"
-credhub set -n ${BBL_ENV_NAME}/bosh_url -t value -v "${BOSH_ENVIRONMENT}"
-credhub set -n ${BBL_ENV_NAME}/bosh_username -t value -v "${BOSH_CLIENT}"
-credhub set -n ${BBL_ENV_NAME}/bosh_password -t value -v "${BOSH_CLIENT_SECRET}"
-credhub set -n ${BBL_ENV_NAME}/bosh_ca_cert -t value -v "${BOSH_CA_CERT}"
+CH="/bosh-bbl-env-${BBL_ENV}"
+CHP="${CH}/prometheus"
+credhub set -n ${CHP}/bosh_url -t value -v "${BOSH_ENVIRONMENT}"
+credhub set -n ${CHP}/bosh_username -t value -v "${BOSH_CLIENT}"
+credhub set -n ${CHP}/bosh_password -t value -v "${BOSH_CLIENT_SECRET}"
+credhub set -n ${CHP}/bosh_ca_cert -t value -v "${BOSH_CA_CERT}"
+
+PW=$(credhub get -n ${CH}/cf/uaa_clients_cf_exporter_secret | grep ^value | awk '{print $2}')
+credhub set -n ${CHP}/uaa_clients_cf_exporter_secret -t value -v ${PW}
+
+PW=$(credhub get -n ${CH}/cf/uaa_clients_firehose_exporter_secret | grep ^value | awk '{print $2}')
+credhub set -n ${CHP}/uaa_clients_firehose_exporter_secret -t value -v ${PW}
