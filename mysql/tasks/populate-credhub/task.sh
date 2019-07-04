761c5e47eafe11e4464f7cb70a542874b2989b05
Alexey Zakharov <alexey.zakharov@altoros.com>
2019-07-04 16:44:51 +0300

    Fix credhub path

--- mysql/tasks/populate-credhub/task.sh
+++ mysql/tasks/populate-credhub/task.sh
@@ -3,5 +3,5 @@
 pushd bbl-state/${BBL_ENV}/state
 eval "$(bbl print-env)"
 popd
-CFPSW=$(credhub get -n /bosh-bbl-env-${BBL_ENV}/cf/cf_admin_password | grep ^value | awk '{print $2}')
-credhub set -n /bosh-bbl-env-${BBL_ENV}/prometheus/cf_admin_password -t value -v $CFPSW
+CFPSW=$(credhub get -n /bosh-${BBL_ENV}/cf/cf_admin_password | grep ^value | awk '{print $2}')
+credhub set -n /bosh-${BBL_ENV}/prometheus/cf_admin_password -t value -v $CFPSW
