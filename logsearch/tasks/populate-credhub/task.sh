b8a66c9eb234f7d71d8754659dea40082a37b3f3
Alexey Zakharov <alexey.zakharov@altoros.com>
2020-01-14 16:19:05 +0300

    Added kibana secret

--- logsearch/tasks/populate-credhub/task.sh
+++ logsearch/tasks/populate-credhub/task.sh
@@ -7,9 +7,14 @@ popd
 FROM="/bosh-${BBL_ENV}/cf"
 TO="/bosh-${BBL_ENV}/logsearch"
 
-CF_PASS=$(credhub get -n ${FROM}/cf_admin_password | grep ^value | awk '{print $2}')
-credhub set -n ${TO}/cf_admin_password -t value -v ${CF_PASS}
-UAA_PASS=$(credhub get -n ${FROM}/uaa_admin_client_secret | grep ^value | awk '{print $2}')
-credhub set -n ${TO}/uaa_admin_client_secret -t value -v ${UAA_PASS}
+for KEY in \
+  cf_admin_password \
+  uaa_admin_client_secret \
+  kibana_oauth2_client_secret;
+  do
+  VALUE=$(credhub get -n ${FROM}/${KEY} | grep ^value | awk '{print $2}')
+  credhub set -n ${TO}/${KEY} -t value -v ${VALUE}
+done
+
 credhub generate -n ${TO}/cf-kibana_client_secret -t password -l 10
 credhub set -n ${TO}/system_domain -t value -v ${SYSTEM_DOMAIN}
